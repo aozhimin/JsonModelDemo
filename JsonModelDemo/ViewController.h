3af766a6c8470263014ee8cc0dc897887ad13546
dev-aozhimin <18818237826@163.com>
2015-09-01 10:50:33 +0800

    Initial Commit

---
+++ JsonModelDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JsonModelDemo
+//
+//  Created by dev-aozhimin on 15/9/1.
+//  Copyright (c) 2015年 dev-aozhimin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JsonModelDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  JsonModelDemo
+//
+//  Created by dev-aozhimin on 15/9/1.
+//  Copyright (c) 2015年 dev-aozhimin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
